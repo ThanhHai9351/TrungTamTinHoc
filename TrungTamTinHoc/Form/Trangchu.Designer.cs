08f7b59d10dd3c7f54dc9291a1828ac81b3317c1
ThanhHai9351 <haihailua123456@gmail.com>
2023-09-25 10:21:01 +0700

    upload

--- TrungTamTinHoc/Form/Trangchu.Designer.cs
+++ TrungTamTinHoc/Form/Trangchu.Designer.cs
@@ -29,103 +29,188 @@ namespace TrungTamTinHoc
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.hệThốngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.quảnLýDữLiệuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.quảnLýThiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.thốngKêToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.traCứuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.hỗTrợToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.menuStrip1.SuspendLayout();
+            this.panelHeading = new System.Windows.Forms.Panel();
+            this.btnThongBao = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnSupport = new System.Windows.Forms.Button();
+            this.btnLichHoc = new System.Windows.Forms.Button();
+            this.btnQLKhoaHoc = new System.Windows.Forms.Button();
+            this.btnQLGiaoVien = new System.Windows.Forms.Button();
+            this.btnQLHV = new System.Windows.Forms.Button();
+            this.btnTrangChu = new System.Windows.Forms.Button();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.panelHeading.SuspendLayout();
+            this.panelMenu.SuspendLayout();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // panelHeading
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.hệThốngToolStripMenuItem,
-            this.quảnLýDữLiệuToolStripMenuItem,
-            this.quảnLýThiToolStripMenuItem,
-            this.thốngKêToolStripMenuItem,
-            this.traCứuToolStripMenuItem,
-            this.hỗTrợToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.panelHeading.BackColor = System.Drawing.Color.DimGray;
+            this.panelHeading.Controls.Add(this.btnThongBao);
+            this.panelHeading.Controls.Add(this.panel2);
+            this.panelHeading.Controls.Add(this.label1);
+            this.panelHeading.Location = new System.Drawing.Point(0, 0);
+            this.panelHeading.Name = "panelHeading";
+            this.panelHeading.Size = new System.Drawing.Size(674, 60);
+            this.panelHeading.TabIndex = 1;
             // 
-            // hệThốngToolStripMenuItem
+            // btnThongBao
             // 
-            this.hệThốngToolStripMenuItem.Name = "hệThốngToolStripMenuItem";
-            this.hệThốngToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
-            this.hệThốngToolStripMenuItem.Text = "Hệ Thống";
-            this.hệThốngToolStripMenuItem.Click += new System.EventHandler(this.hệThốngToolStripMenuItem_Click);
+            this.btnThongBao.Location = new System.Drawing.Point(493, 12);
+            this.btnThongBao.Name = "btnThongBao";
+            this.btnThongBao.Size = new System.Drawing.Size(75, 32);
+            this.btnThongBao.TabIndex = 8;
+            this.btnThongBao.Text = "Thông báo";
+            this.btnThongBao.UseVisualStyleBackColor = true;
             // 
-            // quảnLýDữLiệuToolStripMenuItem
+            // panel2
             // 
-            this.quảnLýDữLiệuToolStripMenuItem.Name = "quảnLýDữLiệuToolStripMenuItem";
-            this.quảnLýDữLiệuToolStripMenuItem.Size = new System.Drawing.Size(130, 24);
-            this.quảnLýDữLiệuToolStripMenuItem.Text = "Quản Lý Dữ Liệu";
+            this.panel2.Location = new System.Drawing.Point(0, 57);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(674, 49);
+            this.panel2.TabIndex = 2;
             // 
-            // quảnLýThiToolStripMenuItem
+            // label1
             // 
-            this.quảnLýThiToolStripMenuItem.Name = "quảnLýThiToolStripMenuItem";
-            this.quảnLýThiToolStripMenuItem.Size = new System.Drawing.Size(99, 24);
-            this.quảnLýThiToolStripMenuItem.Text = "Quản Lý Thi";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(40, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GRADE CDA";
             // 
-            // thốngKêToolStripMenuItem
+            // panelMenu
             // 
-            this.thốngKêToolStripMenuItem.Name = "thốngKêToolStripMenuItem";
-            this.thốngKêToolStripMenuItem.Size = new System.Drawing.Size(86, 24);
-            this.thốngKêToolStripMenuItem.Text = "Thống Kê";
-            this.thốngKêToolStripMenuItem.Click += new System.EventHandler(this.thốngKêToolStripMenuItem_Click);
+            this.panelMenu.BackColor = System.Drawing.Color.DarkSalmon;
+            this.panelMenu.Controls.Add(this.btnSupport);
+            this.panelMenu.Controls.Add(this.btnLichHoc);
+            this.panelMenu.Controls.Add(this.btnQLKhoaHoc);
+            this.panelMenu.Controls.Add(this.btnQLGiaoVien);
+            this.panelMenu.Controls.Add(this.btnQLHV);
+            this.panelMenu.Controls.Add(this.btnTrangChu);
+            this.panelMenu.Location = new System.Drawing.Point(0, 60);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(674, 43);
+            this.panelMenu.TabIndex = 2;
             // 
-            // traCứuToolStripMenuItem
+            // btnSupport
             // 
-            this.traCứuToolStripMenuItem.Name = "traCứuToolStripMenuItem";
-            this.traCứuToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
-            this.traCứuToolStripMenuItem.Text = "Tra Cứu";
+            this.btnSupport.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnSupport.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSupport.Location = new System.Drawing.Point(546, -3);
+            this.btnSupport.Name = "btnSupport";
+            this.btnSupport.Size = new System.Drawing.Size(116, 46);
+            this.btnSupport.TabIndex = 7;
+            this.btnSupport.Text = "Hỗ Trợ";
+            this.btnSupport.UseVisualStyleBackColor = false;
+            this.btnSupport.Click += new System.EventHandler(this.btnSupport_Click);
             // 
-            // hỗTrợToolStripMenuItem
+            // btnLichHoc
             // 
-            this.hỗTrợToolStripMenuItem.Name = "hỗTrợToolStripMenuItem";
-            this.hỗTrợToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
-            this.hỗTrợToolStripMenuItem.Text = "Hỗ Trợ";
+            this.btnLichHoc.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnLichHoc.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnLichHoc.Location = new System.Drawing.Point(435, -3);
+            this.btnLichHoc.Name = "btnLichHoc";
+            this.btnLichHoc.Size = new System.Drawing.Size(118, 46);
+            this.btnLichHoc.TabIndex = 4;
+            this.btnLichHoc.Text = "Lịch Học";
+            this.btnLichHoc.UseVisualStyleBackColor = false;
+            this.btnLichHoc.Click += new System.EventHandler(this.btnLichHoc_Click);
             // 
-            // contextMenuStrip1
+            // btnQLKhoaHoc
             // 
-            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.btnQLKhoaHoc.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnQLKhoaHoc.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnQLKhoaHoc.Location = new System.Drawing.Point(327, -3);
+            this.btnQLKhoaHoc.Name = "btnQLKhoaHoc";
+            this.btnQLKhoaHoc.Size = new System.Drawing.Size(116, 46);
+            this.btnQLKhoaHoc.TabIndex = 6;
+            this.btnQLKhoaHoc.Text = "Quản Lý Khóa Học";
+            this.btnQLKhoaHoc.UseVisualStyleBackColor = false;
+            this.btnQLKhoaHoc.Click += new System.EventHandler(this.btnQLKhoaHoc_Click);
+            // 
+            // btnQLGiaoVien
+            // 
+            this.btnQLGiaoVien.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnQLGiaoVien.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnQLGiaoVien.Location = new System.Drawing.Point(218, -3);
+            this.btnQLGiaoVien.Name = "btnQLGiaoVien";
+            this.btnQLGiaoVien.Size = new System.Drawing.Size(115, 46);
+            this.btnQLGiaoVien.TabIndex = 5;
+            this.btnQLGiaoVien.Text = "Quản Lý Giáo Viên";
+            this.btnQLGiaoVien.UseVisualStyleBackColor = false;
+            this.btnQLGiaoVien.Click += new System.EventHandler(this.btnQLGiaoVien_Click);
+            // 
+            // btnQLHV
+            // 
+            this.btnQLHV.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnQLHV.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnQLHV.Location = new System.Drawing.Point(107, -3);
+            this.btnQLHV.Name = "btnQLHV";
+            this.btnQLHV.Size = new System.Drawing.Size(116, 46);
+            this.btnQLHV.TabIndex = 4;
+            this.btnQLHV.Text = "Quản Lý Học Viên";
+            this.btnQLHV.UseVisualStyleBackColor = false;
+            this.btnQLHV.Click += new System.EventHandler(this.btnQLHV_Click);
+            // 
+            // btnTrangChu
+            // 
+            this.btnTrangChu.BackColor = System.Drawing.Color.LightCoral;
+            this.btnTrangChu.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnTrangChu.Location = new System.Drawing.Point(0, -3);
+            this.btnTrangChu.Name = "btnTrangChu";
+            this.btnTrangChu.Size = new System.Drawing.Size(113, 46);
+            this.btnTrangChu.TabIndex = 3;
+            this.btnTrangChu.Text = "Trang chủ";
+            this.btnTrangChu.UseVisualStyleBackColor = false;
+            this.btnTrangChu.Click += new System.EventHandler(this.btnTrangChu_Click);
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelContainer.Location = new System.Drawing.Point(0, 100);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(674, 310);
+            this.panelContainer.TabIndex = 3;
+            this.panelContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContainer_Paint);
             // 
             // Trangchu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.ClientSize = new System.Drawing.Size(674, 410);
+            this.Controls.Add(this.panelContainer);
+            this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.panelHeading);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Trangchu";
             this.Text = "Trangchu";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.Load += new System.EventHandler(this.Trangchu_Load);
+            this.panelHeading.ResumeLayout(false);
+            this.panelHeading.PerformLayout();
+            this.panelMenu.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem hệThốngToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem quảnLýDữLiệuToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem quảnLýThiToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem thốngKêToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem traCứuToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem hỗTrợToolStripMenuItem;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel panelHeading;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnQLGiaoVien;
+        private System.Windows.Forms.Button btnQLHV;
+        private System.Windows.Forms.Button btnTrangChu;
+        private System.Windows.Forms.Button btnLichHoc;
+        private System.Windows.Forms.Button btnQLKhoaHoc;
+        private System.Windows.Forms.Button btnSupport;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Button btnThongBao;
     }
 }
---
+++ TrungTamTinHoc/UserControls/QLGV.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace TrungTamTinHoc.UserControls
+{
+    partial class QLGV
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản lý giáo viên";
+            // 
+            // QLGV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "QLGV";
+            this.Size = new System.Drawing.Size(340, 235);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
